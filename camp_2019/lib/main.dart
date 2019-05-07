98330691464947886e4c4abbec93d13b07e64fac
Marius Muntean <marius.muntean@zuehlke.com>
2019-05-07 14:24:33 +0200

    Set up Flutter projects

---
+++ camp_2019/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Popcorn Maker',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: MyHomePage(title: 'Popcorn Maker'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.title)),
+        body: Center(
+          child: RaisedButton(
+            child: Text("I don't do anything"),
+            onPressed: () => {setState(() => {})},
+          ),
+        ));
+  }
+}
