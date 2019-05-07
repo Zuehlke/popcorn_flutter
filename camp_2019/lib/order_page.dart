b8fa03c62d7ab6262e7bb91d2ffbdc2a669b7823
Daniel Rosendorf <daniel.rosendorf@zuehlke.com>
2019-05-07 17:22:53 +0200

    Add Amount and Flavour selection

--- camp_2019/lib/order_page.dart
+++ camp_2019/lib/order_page.dart
@@ -9,12 +9,46 @@ class OrderPage extends StatefulWidget {
 }
 
 class OrderPageState extends State<OrderPage> {
+  Flavours _selectedFlavour;
+
+  RadioListTile<Flavours> createFlavourRadioButton(Flavours flavourValue) {
+    var flavourString = flavourValue.toString();
+    flavourString = flavourString.substring(flavourString.indexOf('.') + 1);
+
+    return RadioListTile<Flavours>(
+      title: Text(flavourString),
+      value: flavourValue,
+      groupValue: _selectedFlavour,
+      onChanged: (Flavours value) {
+        setState(() {
+          _selectedFlavour = value;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new Scaffold(
-        body: new Center(
-      child: Text('OrderPage'),
-    ));
+        appBar: new AppBar(
+          title: Text('Order Popcorn'),
+        ),
+        body: Padding(
+          child: new Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount (in g)'),
+              ),
+              Text('Flavour:'),
+              createFlavourRadioButton(Flavours.Salty),
+              createFlavourRadioButton(Flavours.Sweet),
+              createFlavourRadioButton(Flavours.Caramel),
+              createFlavourRadioButton(Flavours.Wasabi),
+            ],
+          ),
+          padding: EdgeInsets.all(5),
+        ));
   }
 }
+
+enum Flavours { Salty, Sweet, Caramel, Wasabi }
