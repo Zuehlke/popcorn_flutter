0ab400aa1c8e7be43f9676e2cb5def99c7210b1a
Fahed Jibril <fji@zuehlke.com>
2019-05-07 18:46:45 +0200

    Implement spike by sending post to Azure on button click

--- camp_2019/lib/main.dart
+++ camp_2019/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,31 +37,72 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar: AppBar(title: Text(widget.title)),
         body: Center(
-          child: Column(
-            children: [
-              RaisedButton(
-                child: Text("Spike"),
-                onPressed: () => {setState(() => {})},
-              ),
-              RaisedButton(
-                child: Text("Main"),
-                onPressed: () => {setState(() => {})},
-              ),
-              RaisedButton(
-                child: Text("Order"),
-                onPressed: () => {setState(() => {})},
-              ),
-              RaisedButton(
-                child: Text("OrderDetails"),
-                onPressed: () => {setState(() => {})},
-              ),
-              RaisedButton(
+          child: Column(children: [
+            RaisedButton(
+              child: Text("Spike"),
+              onPressed: () => {
+                    setState(() => {
+                          Spike.sendRequest(42)
+                              .then((int orderId) => {
+                                    Fluttertoast.showToast(
+                                        msg:
+                                            "Successfully sent request and received back orderId " +
+                                                orderId.toString(),
+                                        toastLength: Toast.LENGTH_SHORT,
+                                        timeInSecForIos: 1,
+                                        backgroundColor: Colors.red,
+                                        textColor: Colors.white,
+                                        fontSize: 16.0)
+                                  })
+                              .catchError((error) => {
+                                    Fluttertoast.showToast(
+                                        msg: error.toString(),
+                                        toastLength: Toast.LENGTH_SHORT,
+                                        timeInSecForIos: 1,
+                                        backgroundColor: Colors.red,
+                                        textColor: Colors.white,
+                                        fontSize: 16.0)
+                                  })
+                        })
+                  },
+            ),
+            RaisedButton(
+              child: Text("Main"),
+              onPressed: () => {setState(() => {})},
+            ),
+            RaisedButton(
+              child: Text("Order"),
+              onPressed: () => {setState(() => {})},
+            ),
+            RaisedButton(
+              child: Text("OrderDetails"),
+              onPressed: () => {setState(() => {})},
+            ),
+            RaisedButton(
               child: Text("Settings"),
               onPressed: () => {setState(() => {})},
-              ),
-          ]
-        ),
-    )
-    );
+            ),
+          ]),
+        ));
+  }
+}
+
+class Spike {
+  static Future<int> sendRequest(int machineId) async {
+    var data = {"userName": "FlutterSpike", "amount": 100, "flavour": "SWEET"};
+    var url = 'https://popcornmakerbackend20190507022416.azurewebsites.net/api';
+    url += '/machines/' + machineId.toString() + '/orders';
+    var headers = {'Content-Type': 'application/json'};
+    var response = await http.post(url, body: json.encode(data), headers: headers);
+
+    final int statusCode = response.statusCode;
+
+    if (statusCode != 201) {
+      throw response.body;
+    }
+
+    var responseJson = json.decode(response.body);
+    return responseJson['orderId'] as int;
+    // return int.parse(responseJson[orderId] as int);
   }
 }
