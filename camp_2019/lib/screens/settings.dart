80487a2e15a1fc74cc11bdfe9aeed26cb827bce4
Marius Muntean <marius.muntean@zuehlke.com>
2019-05-07 18:17:59 +0200

    Added the a text field to the Settings page that is persisted and restored from the user preferences

--- camp_2019/lib/screens/settings.dart
+++ camp_2019/lib/screens/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -6,13 +7,41 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  final userNamePrefKey = "userName";
+  TextEditingController _usernameController = TextEditingController();
+
+  @override
+  void initState() {
+    SharedPreferences.getInstance().then((preferences) {
+      if (preferences.containsKey(userNamePrefKey)) {
+        _usernameController.text = preferences.get(userNamePrefKey);
+      }
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Settings")),
-      body: Center( child:
-        Column()
-      )
-    );
+        appBar: AppBar(title: Text("Settings")),
+        body: Container(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: <Widget>[
+                Text("Name"),
+                TextField(
+                  decoration: InputDecoration(hintText: "Please enter your "
+                      "name"),
+                  maxLines: 1,
+                  onChanged: (newText) {
+                    SharedPreferences.getInstance().then((preferences) {
+                      preferences.setString(userNamePrefKey, newText);
+                    });
+                  },
+                  controller: _usernameController,
+                )
+              ],
+            )));
   }
 }
