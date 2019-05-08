00424dbb6bdc32035982eb22de38620c66c886b7
Marius Muntean <marius.muntean@zuehlke.com>
2019-05-08 12:13:20 +0200

    Added machine dropdown, status and queue to the page

--- camp_2019/lib/main.dart
+++ camp_2019/lib/main.dart
@@ -24,20 +24,24 @@ class MainPage extends StatefulWidget {
   final String title;
 
   @override
-  _MainPageState createState() => _MainPageState(["mid: 01", "mid: 02", "mid: 03"]);
+  _MainPageState createState() => _MainPageState(
+      ["mid: 01", "mid: 02", "mid: 03"], ["O1", "O2", "O3", "O1", "O2", "O3", "O1", "O2", "O3", "O1", "O2", "O3", "O1", "O2", "O3", "O1", "O2", "O3"]);
 }
 
 class _MainPageState extends State<MainPage> {
+  final _biggerFont = const TextStyle(fontSize: 18);
+
   final List<String> _machines;
+  final List<String> _orders;
   String selectedMachine;
 
-  _MainPageState(this._machines) : super()
-  {
+  _MainPageState(this._machines, this._orders) : super() {
     selectedMachine = _machines[0];
   }
 
   @override
   Widget build(BuildContext context) {
+    final borderSide = BorderSide(color: Colors.grey, width: 1.0, style: BorderStyle.solid);
     return Scaffold(
         appBar: AppBar(
           title: Text(
@@ -49,47 +53,100 @@ class _MainPageState extends State<MainPage> {
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.settings),
-              onPressed: () =>  Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SettingsPage())),
+              onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage())),
               color: Colors.white,
             ),
           ],
         ),
         body: Center(
-          child: Column(children: [
-            DropdownButton<String>(
-                items: _machines.map<DropdownMenuItem<String>>((value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            onChanged: (newValue) => {
-                  setState(() {
-                    selectedMachine = newValue;
-                  })
-            },
-            value: selectedMachine,
-            ),
-            RaisedButton(
-              child: Text("Spike"),
-              onPressed: () => {},
-            ),
-            RaisedButton(
-              child: Text("Order"),
-              onPressed: () => {},
-            ),
-            RaisedButton(
-              child: Text("OrderDetails"),
-              onPressed: () => {},
-            ),
-          ]),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+              // Machine dropdown
+
+              Container(
+                decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+                child: DropdownButton<String>(
+                  items: _machines.map<DropdownMenuItem<String>>((value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) => {
+                        setState(() {
+                          selectedMachine = newValue;
+                        })
+                      },
+                  value: selectedMachine,
+                ),
+              ),
+
+              // Machine status
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Table(
+                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                  border: TableBorder(bottom: borderSide, top: borderSide, left: borderSide, right: borderSide, horizontalInside: borderSide),
+                  children: [
+                    TableRow(children: [Text("Machine State"), Text("BUSY")]),
+                    TableRow(children: [Text("Corn Level"), Text("81%")]),
+                    TableRow(children: [Text("Flavours"), Text("SWEET, SALTY, CARAMEL, WASABI")])
+                  ],
+                ),
+              ),
+
+              // Machine Order Queue
+              Align(
+                heightFactor: 1.5,
+                child: Text(
+                  "Queue",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                alignment: Alignment.bottomLeft,
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                      border: Border(top: borderSide, right: borderSide, left: borderSide, bottom: borderSide),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  child: Scrollbar(
+                    child: ListView.separated(
+                      padding: EdgeInsets.all(6),
+                      itemCount: _orders.length,
+                      separatorBuilder: (context, index) => Divider(),
+                      itemBuilder: (context, i) {
+                        return Text(
+                          _orders[i],
+                          style: _biggerFont,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+
+              RaisedButton(
+                child: Text("Spike"),
+                onPressed: () => {},
+              ),
+              RaisedButton(
+                child: Text("Order"),
+                onPressed: () => {},
+              ),
+              RaisedButton(
+                child: Text("OrderDetails"),
+                onPressed: () => {},
+              ),
+            ]),
+          ),
         ));
   }
 
-  RaisedButton buildNavigationalButton(String buttonText,
-      StatefulWidget destinationBuilder(BuildContext context)) {
+  RaisedButton buildNavigationalButton(String buttonText, StatefulWidget destinationBuilder(BuildContext context)) {
     return RaisedButton(
       child: Text(buttonText),
       onPressed: () {
