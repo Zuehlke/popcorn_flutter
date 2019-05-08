b74fe7ea578b478aa77de0372e18e309912b21c5
Ben Lindner <blindner@brabbler.ag>
2019-05-08 18:48:36 +0200

    satisfy flutter analyze

--- camp_2019/lib/models/amount.dart
+++ camp_2019/lib/models/amount.dart
@@ -1,10 +1,5 @@
 class Amount {
-  int _value;
-
-  Amount(this._value) {}
-
   static bool isValid(int amount) => amount >= minValue && amount <= maxValue;
-
   static final int minValue = 5;
   static final int maxValue = 500;
 }
