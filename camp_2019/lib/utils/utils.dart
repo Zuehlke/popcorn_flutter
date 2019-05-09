4b08cb47a725fad44f45560c14b58fc7464f62bb
Daniel Rosendorf <daniel.rosendorf@zuehlke.com>
2019-05-09 15:32:27 +0200

    Add tests for Utils and fix problems

--- camp_2019/lib/utils/utils.dart
+++ camp_2019/lib/utils/utils.dart
@@ -1,20 +1,30 @@
 class Utils {
   static DateTime parseDateTime(String date) {
+    if (date == null) {
+      return null;
+    }
+
     var fractionCharacterPosition = date.indexOf(".");
     var offsetCharacterPosition = date.indexOf("+");
+    var dateToParse;
 
     if (fractionCharacterPosition == -1) {
-      return DateTime.parse(date);
+      dateToParse = date;
+      return DateTime.parse(dateToParse);
     }
 
     if (offsetCharacterPosition == -1) {
-      return DateTime.parse(
-          date.substring(fractionCharacterPosition + 1).substring(0, 6));
+      dateToParse = date.substring(0, fractionCharacterPosition + 1) +
+          date.substring(fractionCharacterPosition + 1).substring(0, 6);
+      return DateTime.parse(dateToParse);
     }
 
-    return DateTime.parse(date
-        .substring(fractionCharacterPosition + 1, offsetCharacterPosition)
-        .substring(0, 6));
+    dateToParse = date.substring(0, fractionCharacterPosition + 1) +
+        date
+            .substring(fractionCharacterPosition + 1, offsetCharacterPosition)
+            .substring(0, 6) +
+        date.substring(offsetCharacterPosition);
+    return DateTime.parse(dateToParse);
 
     // see https://stackoverflow.com/questions/52789217/flutter-parsing-json-with-datetime-from-golang-rfc3339-formatexception-invalid
     // basically dart can't handle more than 6 fraction digits but the specification says 7
---
+++ camp_2019/test/utils/utils_test.dart
@@ -0,0 +1,45 @@
+import 'package:camp_2019/utils/utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Utils parseDateTime', () {
+    test('parse null value', () {
+      final result = Utils.parseDateTime(null);
+
+      expect(result, null);
+    });
+
+    test('parse valid value with TZ offset', () {
+      final result = Utils.parseDateTime('2019-05-09T12:14:46.6716303+00:00');
+
+      expect(result.day, 9);
+      expect(result.month, 5);
+      expect(result.year, 2019);
+      expect(result.hour, 12);
+      expect(result.minute, 14);
+      expect(result.year, 2019);
+    });
+
+    test('parse valid value without TZ offset', () {
+      final result = Utils.parseDateTime('2019-05-09T12:14:46.6716303');
+
+      expect(result.day, 9);
+      expect(result.month, 5);
+      expect(result.year, 2019);
+      expect(result.hour, 12);
+      expect(result.minute, 14);
+      expect(result.year, 2019);
+    });
+
+    test('parse valid value without TZ offset', () {
+      final result = Utils.parseDateTime('2019-05-09T12:14:46');
+
+      expect(result.day, 9);
+      expect(result.month, 5);
+      expect(result.year, 2019);
+      expect(result.hour, 12);
+      expect(result.minute, 14);
+      expect(result.year, 2019);
+    });
+  });
+}
