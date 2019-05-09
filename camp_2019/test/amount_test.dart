d16f63807a03df8ce0d20e684c4b776eadcf6e7f
Daniel Rosendorf <daniel.rosendorf@zuehlke.com>
2019-05-09 09:58:56 +0200

    Add basic amount tests

--- camp_2019/test/amount_test.dart
+++ camp_2019/test/amount_test.dart
@@ -1,7 +1,26 @@
+import 'package:camp_2019/amount.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Counter value should be incremented', () {
-    expect(1, 2);
+  group('Amount isValid tests', () {
+    test('amount 4 is invalid', () {
+      expect(Amount.isValid(4), false);
+    });
+
+    test('amount 501 is invalid', () {
+      expect(Amount.isValid(500), true);
+    });
+
+    test('amount 5 is valid', () {
+      expect(Amount.isValid(5), true);
+    });
+
+    test('amount 500 is valid', () {
+      expect(Amount.isValid(500), true);
+    });
+
+    test('amount 300 is valid', () {
+      expect(Amount.isValid(500), true);
+    });
   });
 }
